73c5107b924f58a5ad6dd667d0d5373f4e477e98
mahmoudodoo <mahmoudodoo11@gmail.com>
2020-11-30 10:48:36 +0200

    Remove Excess files

--- walkOs.py
+++
@@ -1,9 +0,0 @@
-import os
-from os.path import join,getsize
-
-
-
-for root, dirs, files in os.walk('/home/mahmoudodeh/Desktop/Ethical Hacking Python'):
-    print(root, end=" ")
-    print(sum([getsize(join(root,name)) for name in files]),end=" " )
-    print('Bytes in', len(files), 'non-directory files')
